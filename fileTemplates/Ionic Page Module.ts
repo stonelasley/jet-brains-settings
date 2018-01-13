30c5c3397b5d78311381d256dcee537a006869b0
Stone C. Lasley <stonelasley@gmail.com>
2018-01-12 19:25:11 -0700

    WS-2017.3.2 <s@smbp.lasley.local Update vim_settings.xml, ide.general.xml, project.default.xml, JavaScript.xml, Default.xml, file.template.settings.xml, Jasmine.xml, find.xml, phonegap.xml, filetypes.xml, Visual Studio copy.xml, TypeScript.xml, TypesScribe - Jasmine.xml, editor.xml	Create markdown.xml, IntelliLang.xml, Ionic Page Module.ts, other.xml, Ionic.xml, ui.lnf.xml, laf.xml, Default _1_.xml, web-browsers.xml, abbreviations.xml, material_theme.xml, code.style.schemes.xml, TypeScript Spec File༏ext༏.spec.ts, keymap.xml

---
+++ fileTemplates/Ionic Page Module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { $PAGECLASS } from './$CLASSFILE';
+
+@NgModule({
+    declarations: [
+        $PAGECLASS,
+        ],
+  imports: [
+    IonicPageModule.forChild(${PAGECLASS}),
+  ],
+})
+export class ${PAGECLASS}PageModule {}
---
+++ fileTemplates/TypeScript Spec File༏ext༏.spec.ts
@@ -0,0 +1,12 @@
+import { $TYPE$ } from './';
+
+describe('$CLASSUNDERTEST$', () => {
+
+    let _classUnderTest: $TYPE$;
+    
+    beforeEach(() => {
+    
+        _classUnderTest = new $TYPE$();
+    );
+
+});
